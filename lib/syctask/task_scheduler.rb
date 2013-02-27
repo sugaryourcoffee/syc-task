2bb6e6a478049639ad373abb254990c99762a74b
Pierre <pierre@sugaryourcoffee.de>
2013-02-27 22:38:35 +0100

    add quarterly resolution and scanner for free time

--- lib/syctask/task_scheduler.rb
+++ lib/syctask/task_scheduler.rb
@@ -3,8 +3,9 @@ require 'rainbow'
 module Syctask
 
   class TaskScheduler
-    WORK_TIME_PATTERN = /(\d+)-(\d+)/
-    BUSY_TIME_PATTERN = /(\d+)-(\d+)(?=,)|(\d+)-(\d+)$/
+    TIME_PATTERN = /(2[0-3]|[01]?[0-9]):([0-5]?[0-9])/
+    WORK_TIME_PATTERN = /#{TIME_PATTERN}-#{TIME_PATTERN}/ 
+    BUSY_TIME_PATTERN = /#{TIME_PATTERN}-#{TIME_PATTERN}(?=,)|#{TIME_PATTERN}-#{TIME_PATTERN}$/
     GRAPH_PATTERN = /[\|-]+|\/+/
     BUSY_PATTERN = /\/+/
     FREE_PATTERN = /[\|-]+/
@@ -12,12 +13,52 @@ module Syctask
     def initialize(work_time, busy_time)
       @work_time = work_time.scan(WORK_TIME_PATTERN).flatten
       @busy_time = busy_time.scan(BUSY_TIME_PATTERN).each {|busy| busy.compact!}
-      puts @work_time
-      puts @busy_time
-      create_graph(@work_time, @busy_time)
+      if range_is_sequential?
+        normalize_time
+        create_graph(@work_time, @busy_time)
+      end
+    end
+
+    def range_is_sequential?
+      return false unless check_range(@work_time)
+      @busy_time.each do |busy|
+        return false unless check_range(busy)
+      end
+      true
+    end
+
+    def check_range(range)
+      return false unless range[0].to_i < range[2].to_i
+      if range[0].to_i == range[2].to_i
+        return false unless range[1].to_i < range[3].to_i
+      end
+      true
+    end
+ 
+    def normalize_time
+      @graph_ranges = []
+      @graph_ranges[0] = @work_time[0].to_i
+      @graph_ranges[1] = @work_time[3].to_i > 0 ? @work_time[2].succ.to_i : @work_time[2]
+      
+      @busy_ranges = []
+      @busy_time.each do |busy|
+        busy_range = Array.new(2)
+        busy_range[0] = (busy[0].to_i - @graph_ranges[0]) * 4 + minute_offset(busy[1])
+        busy_range[1] = (busy[2].to_i - @graph_ranges[0]) * 4 + minute_offset(busy[3])
+        @busy_ranges << busy_range
+      end
+    end
+
+    def hour_offset(starts, ends)
+      (ends - starts) * 4
+    end
+
+    def minute_offset(minutes)
+      minutes.to_i.div(15)
     end
 
     def print_graph
+      return -1 unless @schedule_graph
       @schedule_graph.scan(GRAPH_PATTERN) do |part|
         print sprintf("%s", part).color(:red) unless part.scan(BUSY_PATTERN).empty?
         print sprintf("%s", part).color(:green) unless part.scan(FREE_PATTERN).empty?
@@ -27,21 +68,35 @@ module Syctask
     end
 
     def create_graph(work_time, busy_time)
-      @schedule_graph = '|---' * (work_time[1].to_i-work_time[0].to_i) + '|'
+      @schedule_graph = '|---' * (@graph_ranges[1]-@graph_ranges[0]) + '|'
 
       @schedule_units = ""
-      work_time[0].to_i.upto(work_time[1].to_i) do |time|
+      @graph_ranges[0].to_i.upto(@graph_ranges[1]) do |time|
         @schedule_units += time.to_s + (time < 9 ? ' ' * 3 : ' ' * 2)
       end
 
-      @busy_time.each do |busy|
-        busy_pattern = '/' * 4 * (busy[1].to_i - busy[0].to_i)
-        busy_start = 1 + 4 * (busy[0].to_i - work_time[0].to_i)
-        busy_end   = busy_start - 1 + busy_pattern.size
-        @schedule_graph[busy_start..busy_end] = busy_pattern 
+      @busy_ranges.each do |busy|
+        @schedule_graph[busy[0]..busy[1]] = '/' * (busy[1] - busy[0]+1)
       end
+
     end
 
+    def scan_free(count)
+      pattern = /(?!\/)[\|-]{#{count}}(?<=-|\||\/)/
+      puts pattern
+      puts @schedule_graph
+
+      positions = []
+      index = 0
+      while index and index < @schedule_graph.size
+        index = @schedule_graph.index(pattern, index)
+        if index
+          positions << index
+          index += 1
+        end
+      end
+      positions
+    end
 
   end
 

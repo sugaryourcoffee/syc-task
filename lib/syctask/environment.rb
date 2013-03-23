f332b9c9f05accfe67392f3b7ed4fd0debd0fdb1
Pierre <pierre@sugaryourcoffee.de>
2013-03-23 18:59:02 +0100

    add make_environment to create syctasks directories. Add constants for application directories and files

--- lib/syctask/environment.rb
+++ lib/syctask/environment.rb
@@ -1,5 +1,15 @@
 module Syctask
 
   WORK_DIR = File.expand_path('~/.tasks')
+  SYC_DIR = File.expand_path('~/.syc/syctask')
+  ID = SYC_DIR + "/id"
+  IDS = SYC_DIR + "/properties/ids"
+  TAGS = SYC_DIR + "/tags"
+  DEFAULT_TASKS = SYC_DIR + "/default_tasks"
+
+  def make_environment
+    FileUtils.mkdir_p WORK_DIR unless File.exists? WORK_DIR
+    FileUtils.mkdir_p SYC_DIR unless File.exists? SYC_DIR
+  end
 
 end

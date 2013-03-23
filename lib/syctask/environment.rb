5803f367880549644061cd4bc549cacfbdda8cf4
Pierre <pierre@sugaryourcoffee.de>
2013-03-23 21:32:11 +0100

    add template of reindex_tasks

--- lib/syctask/environment.rb
+++ lib/syctask/environment.rb
@@ -3,13 +3,20 @@ module Syctask
   WORK_DIR = File.expand_path('~/.tasks')
   SYC_DIR = File.expand_path('~/.syc/syctask')
   ID = SYC_DIR + "/id"
-  IDS = SYC_DIR + "/properties/ids"
+  IDS = SYC_DIR + "/ids"
   TAGS = SYC_DIR + "/tags"
   DEFAULT_TASKS = SYC_DIR + "/default_tasks"
 
   def make_environment
     FileUtils.mkdir_p WORK_DIR unless File.exists? WORK_DIR
     FileUtils.mkdir_p SYC_DIR unless File.exists? SYC_DIR
+  end
+
+  def reindex_tasks
+    root = File.expand_path('~')
+    tasks = get_all_tasks(root)
+    tasks.each_with_index do |task,i|
+    end
   end
 
 end

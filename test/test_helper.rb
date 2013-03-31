2018aea57b987e62c317320a25c8916b845e996a
Pierre <pierre@sugaryourcoffee.de>
2013-03-31 18:45:28 +0200

    add backup_system_files and restore_system_files to be used in all tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,29 @@
 require 'test/unit'
+require 'shoulda'
+require_relative '../lib/syctask/task_scheduler.rb'
+include Syctask
 
-# Add test libraries you want to use here, e.g. mocha
+# Overrides Test::Unit::TestCase to add some helper methods for each test
 class Test::Unit::TestCase 
 
-  # Add global extensions to the test case class here
-  
+  # Backs up system files before manipulating them in the test
+  def backup_system_files
+    system_files = Dir.glob("#{Syctask::SYC_DIR}/*")
+    system_files.each do |f|
+      FileUtils.mv f, f + ".original"
+      FileUtils.touch f
+    end
+  end
+
+  # Restores the file systems after the test
+  def restore_system_files
+    Dir.glob("#{Syctask::SYC_DIR}/*").each do |f|
+      if f.end_with? ".original" 
+        FileUtils.mv f, f.sub(".original", "") 
+      else
+        FileUtils.rm f 
+      end
+    end
+  end
+
 end

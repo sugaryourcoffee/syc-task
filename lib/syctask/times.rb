b3d842e3030c9918e76289dfcd34d4e29ca955c5
Pierre <pierre@sugaryourcoffee.de>
2013-04-07 11:02:11 +0200

    add calcualtion of the difference between a given time and this Times object

--- lib/syctask/times.rb
+++ lib/syctask/times.rb
@@ -20,6 +20,19 @@ module Syctask
       @m > 0 ? @h+1 : @h
     end
 
+    # Calculates the difference between this time and the provided time. If no
+    # time is given the current time is used.
+    #     Example:
+    #     This time  =  9:35
+    #     New time   = 10:20
+    #     diff(time) =  0:45
+    # Will return [hour,min] in the example [0,45] 
+    def diff(time = Time.now)
+      diff_minutes = (time.hour - @h) * 60 + (time.min - @m)
+      diff_h = diff_minutes / 60 % 60
+      diff_m = diff_minutes % 60
+      [diff_h, diff_m]
+    end
   end
 
 end

c92d7781996fcc2c85f7bc2a226d616fba59d3f6
Pierre Sugar <pierre@sugaryourcoffee.de>
2024-12-15 11:36:54 +0100

    Update version number from 0.4.2 to 1.0.0 as the application has been upgraded to run with Ruby 3

--- lib/syctask/version.rb
+++ lib/syctask/version.rb
@@ -1,5 +1,5 @@
 # Syctask provides functions for managing tasks in a task list
 module Syctask
-  #Holds the version number of syctask
-  VERSION = '0.4.2'
+  # Holds the version number of syctask
+  VERSION = '1.0.0'
 end
